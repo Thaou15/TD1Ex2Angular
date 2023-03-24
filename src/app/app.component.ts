3d4fbe0654ac75ccb2d7165d772e55bf2dd642a3
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-24 16:02:53 +0100

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1Ex2';
+
+  imageUrl:string="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg";
+  tailleImage: number=20;
+
+  inf:string="";
+
+  utilisateur: any[] = [
+    {"nom":"Durant", "prenom":"Charles", "email":"d.c@gmail.com", "telephone":"010203", "age":23},
+   
+  ]
+
+  onClick()
+  {
+    console.log ("Bouton cliqué");
+  }
+
+
 }
